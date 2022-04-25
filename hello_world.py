2495f85239d329750829ced9b05ab5bf7a5ea510
Poype <253186926@qq.com>
2022-04-25 12:45:10 +0800

    字符串的一些方法

--- hello_world.py
+++ hello_world.py
@@ -12,3 +12,27 @@ print(message1)
 # 单引号字符串，包含双引号
 message2 = 'he said: "this is a test" '
 print(message2)
+
+# 字符串的一些方法
+message3 = "ada lovelace"
+print(message3.title()) # Ada Lovelace
+
+message4 = 'Ada Lovelace'
+print(message4.upper()) # ADA LOVELACE
+print(message4.lower()) # ada lovelace
+
+message5 = "    test    "
+print(message5.rstrip()) # 只删除右边的空白
+print(message5.lstrip() + "a") # 只删除左边的空白
+print(message5.strip()) # 删除两边的空白
+
+# F字符串，注意这里的变量名没有用驼峰的模式
+first_name = 'Poype'
+last_name = 'Liu'
+full_name = f"{first_name} {last_name}"
+print(full_name) # Poype Liu
+print(f"Hello {full_name}") # Hello Poype Liu
+print(f"Hello {full_name.upper()}") # Hello POYPE LIU
+
+# f字符串是在3.6版本引入的，如果是之前的版本要用format方法
+print("Hello {}".format(full_name)) # Hello Poype Liu
