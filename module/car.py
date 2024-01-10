af49984e84352b126df800eb77cfc0b29c8bea46
poype <253186926@qq.com>
2024-01-10 20:41:01 +0800

    1. Python 模块（Module）就是一个python文件 2. if __name__ == '__main__': 的作用 3. __all__

--- module/car.py
+++ module/car.py
@@ -1,3 +1,9 @@
+__all__ = ["Car"]
+# 如果一个模块文件中有“__all__”变量，则在其它文件中使用“from xxx import *”导入时，只能导入这个列表中的元素
+# 由于Dog类不在这个列表中，所以在其它文件中用“import *”不会导入Dog类
+# 但如果使用“from xxx import Dog”明确导入该类是可以正常import的
+# __all__这个东西应该用处不大
+
 class Car:
 
     def __init__(self, make, model, year):
@@ -5,11 +11,11 @@ class Car:
         self.model = model
         self.year = year
         self.odometer_reading = 0
-    
+
     def get_descriptive_name(self):
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
-    
+
     def read_odometer(self):
         print(f"This car has {self.odometer_reading} miles on it")
 
@@ -21,3 +27,11 @@ class Car:
 
     def increment_odometer(self, miles):
         self.odometer_reading += miles
+
+
+class Dog:
+    pass
+
+
+if __name__ == '__main__':
+    print("in car file")  # 当car.py文件作为module被其它文件import时，__name__变量的值不是__main__，所以这句话不会打印
--- test_module_class.py
+++ test_module_class.py
@@ -1,5 +1,18 @@
 # 从外部模块import一个类
-from module.car import Car
+from module.car import *
+import time
 
 car = Car('audi', 'a4', 2019)
 print(car.get_descriptive_name())
+
+time.sleep(1)
+print("end")
+# Python 模块（Module）就是一个python文件，以.py结尾。模块中可以定义函数，类和变量
+
+# 只有该python文件作为程序入口时__name__变量才会是__main__，如果是作为module被其它文件import，下面的print语句不会执行
+if __name__ == '__main__':
+    print("in main file")
+
+
+# dog = Dog() 会找不到Dog类
+# print(dog)
